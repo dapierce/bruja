e096bfb2332bc418d8fd4ec13fe294913cbedc8c
dapierce <davidaaronpierce@gmail.com>
2018-12-04 11:24:35 -0800

    art auction intro

--- src/components/fundraiser.js
+++ src/components/fundraiser.js
@@ -7,9 +7,18 @@ export default ({ children }) => (
   <div>
     <div id="fundraiser" className={layoutStyles.anchor}></div>
     <div className={fundraiserStyles.container}>
-    <h2 className={fundraiserStyles.header}>Bruja Salon Fundraiser</h2>
-    <p className={fundraiserStyles.copy}>I need your help to make this salon come to life!</p>
-    <h3 className={fundraiserStyles.header}>More details to follow...</h3>
+    <div className={fundraiserStyles.header}>
+      <h2>
+        Bruja Salon Fundraiser<br/>
+        <span className={layoutStyles.swirlDecor}>~</span><br/>
+        <span className={layoutStyles.goldType}>Art Auction</span>
+      </h2>
+    </div>
+    <p className={fundraiserStyles.copy}>
+      I need your help to make this salon
+      come to life! I am putting my artwork up for auction, check back soon for
+      details!
+    </p>
   </div>
   </div>
 )
