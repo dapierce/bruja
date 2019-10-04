4aca2fb7e6a1156a7b1cd3ed132565cd7f4915bf
David Pierce <davidaaronpierce@gmail.com>
2019-10-04 22:58:18 +0000

    Add site map to gatsby-config * First step to decentralize nav links. * In future, top navbar, mobile slide menu and footer site map can all be updated with one change.

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,36 @@ module.exports = {
     description: `Magic is in the hair! Conjuring the best you in Seattle's Greenwood neighborhood. We offer the full range of hair services from cuts to high fashion color.`,
     facebook: `brujahairsalon`,
     instagram: `@bruja_hair`,
+    siteMap: [
+      {
+         name:'Home',
+         link:'/'
+      },
+      {
+         name:'Stylists',
+         link:'/stylists'
+      },
+      {
+         name:'Services',
+         link:'/services'
+      },
+      {
+         name:'Products',
+         link:'/products'
+      },
+      {
+         name:'Shop',
+         link:'https://app.saloninteractive.com/cel/Brujasalon'
+      },
+      {
+         name:'Blog',
+         link:'/blog'
+      },
+      {
+         name:'Book Now',
+         link:'/book-now'
+      },
+    ],
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
