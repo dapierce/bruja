594fcb84ce2a5099fc78d6dcf7312d6ab5778f0a
David Pierce <davidaaronpierce@gmail.com>
2020-06-02 17:47:22 -0700

    Removal of instagram api during build process. - Currently crashes build process on deploy.

--- src/components/instagram.js
+++ src/components/instagram.js
@@ -1,53 +1,53 @@
-// Instagram.js
-import React from "react"
-import { graphql, StaticQuery } from "gatsby"
-import Image from "gatsby-image"
+// // Instagram.js
+// import React from "react"
+// import { graphql, StaticQuery } from "gatsby"
+// import Image from "gatsby-image"
 
-const Instagram = () => (
-  <StaticQuery
-    query={graphql`
-      {
-        allInstaNode(limit: 9, sort: { order: DESC, fields: timestamp }) {
-          edges {
-            node {
-              id
-              caption
-              localFile {
-                childImageSharp {
-                  fluid(maxWidth: 250, maxHeight: 250, quality: 40) {
-                    ...GatsbyImageSharpFluid_withWebp
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    `}
-    render={data => (
-      <div className="gallery">
-        {data.allInstaNode.edges.map((item, i) =>
-          item.node.localFile ? (
-            <div key={i}>
-              <a href={"https://www.instagram.com/p/" + item.node.id}>
-                <Image
-                  className="gallery__image"
-                  fluid={item.node.localFile.childImageSharp.fluid}
-                  alt={
-                    !item.node.caption
-                      ? "Instagram feed photo"
-                      : item.node.caption
-                  }
-                />
-              </a>
-            </div>
-          ) : (
-            <div></div>
-          )
-        )}
-      </div>
-    )}
-  />
-)
+// const Instagram = () => (
+//   <StaticQuery
+//     query={graphql`
+//       {
+//         allInstaNode(limit: 9, sort: { order: DESC, fields: timestamp }) {
+//           edges {
+//             node {
+//               id
+//               caption
+//               localFile {
+//                 childImageSharp {
+//                   fluid(maxWidth: 250, maxHeight: 250, quality: 40) {
+//                     ...GatsbyImageSharpFluid_withWebp
+//                   }
+//                 }
+//               }
+//             }
+//           }
+//         }
+//       }
+//     `}
+//     render={data => (
+//       <div className="gallery">
+//         {data.allInstaNode.edges.map((item, i) =>
+//           item.node.localFile ? (
+//             <div key={i}>
+//               <a href={"https://www.instagram.com/p/" + item.node.id}>
+//                 <Image
+//                   className="gallery__image"
+//                   fluid={item.node.localFile.childImageSharp.fluid}
+//                   alt={
+//                     !item.node.caption
+//                       ? "Instagram feed photo"
+//                       : item.node.caption
+//                   }
+//                 />
+//               </a>
+//             </div>
+//           ) : (
+//             <div></div>
+//           )
+//         )}
+//       </div>
+//     )}
+//   />
+// )
 
-export default Instagram
+// export default Instagram
