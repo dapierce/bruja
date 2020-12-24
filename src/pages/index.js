445dc5221a41105e1c4169c598942e323a05eaee
David Pierce <davidaaronpierce@gmail.com>
2020-12-24 00:03:41 +0000

    Update index.js with one additional whitespace.

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,7 +37,7 @@ const IndexPage = () => (
             <h3>We've Moved!</h3>
             <p>
               A great new opportunity has presented itself in Seattle's Phinney
-              Ridge neighborhood! We have moved the salon over, and are now accepting appointments at
+              Ridge neighborhood! We have moved the salon over, and are now accepting appointments at 
               <a href="https://www.google.com/maps/place/Bruja+Hair+Salon/@47.6764528,-122.356326,17z/data=!3m1!4b1!4m5!3m4!1s0x549015e3cb618539:0xde7c422f4b3c7cfc!8m2!3d47.6764528!4d-122.3541373">our new address</a>.
               <br />
               Thank you for your continued support!
